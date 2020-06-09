943fa3012e2a00aa1687965d09b1169d69ebb784
ElbekDeveloper <e.normurodov@mail.ru>
2020-06-09 21:57:05 +0500

    implement the functionality

---
+++ TextFilesDemo/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFilesDemo
+{
+    class Person
+    {
+        //properties
+        public string  Name { get; set; }
+        public string PhoneCode { get; set; }
+
+        public string  PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        //constructor
+        public Person(string Name, string PhoneCode, string PhoneNumber, string Address)
+        {
+            this.Name = Name;
+            this.PhoneCode = PhoneCode;
+            this.PhoneNumber = PhoneNumber;
+            this.Address = Address;
+        }
+    }
+}
--- TextFilesDemo/Program.cs
+++ TextFilesDemo/Program.cs
@@ -12,6 +12,25 @@ namespace TextFilesDemo
     {
         static void Main(string[] args)
         {
+            string filePath = @"..\..\TextFile.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            List<Person> people = new List<Person>();
+
+            foreach (var line in lines)
+            {
+                string[] entries = line.Split(' ');
+                Person newPerson = new Person(entries[0], entries[1], entries[2], entries[3]);
+
+                //add to the list of people
+                people.Add(newPerson);
+            }
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{ person.Name } lives in {person.Address}. You can contact him by calling  {person.PhoneCode} {person.PhoneNumber} ");
+            }
+
+            Console.ReadKey();
 
         }
     }
